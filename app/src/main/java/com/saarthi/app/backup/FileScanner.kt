4d4e5e7ec99014b84a3ec29efde0b4b75635cbd6
KALKI9437 <159163635+KALKI9437@users.noreply.github.com>
2026-02-10 13:20:29 +0530

    Update FileScanner.kt

--- app/src/main/java/com/saarthi/app/backup/FileScanner.kt
+++ app/src/main/java/com/saarthi/app/backup/FileScanner.kt
@@ -1,1 +1,16 @@
 
+package com.saarthi.app.backup
+
+import android.content.Context
+import android.net.Uri
+import androidx.documentfile.provider.DocumentFile
+
+object FileScanner {
+
+    fun scanFolder(ctx: Context, uri: Uri): List<DocumentFile> {
+
+        val folder = DocumentFile.fromTreeUri(ctx, uri)
+
+        return folder?.listFiles()?.toList() ?: emptyList()
+    }
+}
